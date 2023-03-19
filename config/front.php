22c91e55e9d8ac8aba6fbef6a3de5701ac637de1
Miguel Bolivar <mbol8309@gmail.com>
2023-03-19 01:41:50 +0100

    doing stuff

--- config/front.php
+++ config/front.php
@@ -1,7 +1,37 @@
 <?php
-$front_dir=__DIR__.DIRECTORY_SEPARATOR.'../front/';
-$menu = file_get_contents($front_dir.'menu.json');
+$front_dir = realpath(__DIR__ . DIRECTORY_SEPARATOR . '../front/') . DIRECTORY_SEPARATOR;
 
-return [
-    'menu'=>json_decode($menu)
-];
+if (!function_exists('getDirContents')) {
+    function getDirContents($dir, &$results = array())
+    {
+        $files = scandir($dir);
+
+        foreach ($files as $key => $value) {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+            if (!is_dir($path)) {
+                $results[] = $path;
+            } else if ($value != "." && $value != "..") {
+                getDirContents($path, $results);
+                //$results[] = $path;
+            }
+        }
+        return $results;
+    }
+}
+
+$files = getDirContents($front_dir);
+$length = strlen($front_dir);
+$files = array_map(fn ($str) => substr($str, $length), $files);
+$files = array_reduce($files, function ($carry, $item) use ($front_dir) {
+    $path = $front_dir . $item;
+    $item = substr($item, 0, -5);
+    $item = str_replace(DIRECTORY_SEPARATOR, '.', $item);
+    $carry[$item] = $path;
+    return $carry;
+}, []);
+
+$files = array_map(function ($file) {
+    $content = file_get_contents($file);
+    return json_decode($content, true);
+}, $files);
+return $files;
